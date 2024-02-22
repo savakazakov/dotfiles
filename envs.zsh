c3a83b8bfb4f8401719e872261020684054d9787
Sava Kazakov <savakazakov@gmail.com>
2024-02-22 19:15:15 +0000

    Adding a means to easily add the --no-ignore flag in fd command when using it with fzf

--- envs.zsh
+++ envs.zsh
@@ -5,22 +5,34 @@
 # export MANPATH="/usr/local/man:$MANPATH"
 
 # fzf:
-export FZF_DEFAULT_COMMAND="fd --type f --color=never --hidden"
+# Use FZF_FD_IGNORE = 1 to add the --no-ignore flag to fd.
+# Example: "export FZF_FD_IGNORE=1"
+# A function to use fd with optional --no-ignore and --type
+fd_with_options() {
+  local type_arg="$1"
+  local ignore_flag="$FZF_FD_IGNORE"
+  shift
+
+  if [ "$ignore_flag" = "1" ]; then
+    fd --type "$type_arg" --color=never --hidden --no-ignore "$@"
+  else
+    fd --type "$type_arg" --color=never --hidden "$@"
+  fi
+}
+
+export FZF_DEFAULT_COMMAND="fd_with_options f"
 export FZF_DEFAULT_OPTS="--no-height --reverse --border \
 --color=bg+:#1e1f29,fg+:#f8f8f2,hl:#ff6c6b \
 --color=fg:#f8f8f2,bg:#1e1f29,hl+:#f1fa8c \
 --color=info:#9aedfe,prompt:#5af78e,pointer:#57c7ff \
 --color=marker:#ff92d0,spinner:#5af78e,header:#57c7ff"
 
-# --color=bg+:#282a36,fg+:#f1f1f0,hl:#ff6c6b \
-# --color=fg:#f8f8f2,bg:#1e1f29,hl+:#f1fa8c \
-# --color=info:#9aedfe,prompt:#5af78e,pointer:#57c7ff \
-# --color=marker:#ff92d0,spinner:#5af78e,header:#57c7ff"
-
+# For files (Ctrl-T and default search)
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_CTRL_T_OPTS="--preview 'bat --color=always --line-range :50 {}'"
 
-export FZF_ALT_C_COMMAND="fd --type d --color=never --hidden"
+# For directories (Alt-C)
+export FZF_ALT_C_COMMAND="fd_with_options d"
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -50'"
 
 # bat:
